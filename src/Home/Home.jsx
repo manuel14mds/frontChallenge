344cb5a3f059b20c794cd87d15e52582de46ba6b
manuel14mds <manuel14mds@gmail.com>
2023-08-08 15:48:27 -0500

    feat(home): adding the first challenge step to the react project

---
+++ src/Home/Home.jsx
@@ -0,0 +1,31 @@
+import './Home.css'
+const Home = () => {
+    return (
+        <div className="component">
+
+        <div className="component_image">hols</div>
+
+        <div className="component_data">
+            <h1 className="title">Get <span className="accent">insights</span> that help your business grow.</h1>
+            <p className="info">Discover the benefits of data analystics make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+            <div className="data">
+                <div className="item">
+                    <p className="stats item_info">10K+</p>
+                    <p className="item_info">COMPANIES</p>
+                </div>
+                <div className="item">
+                    <p className="stats item_info">314</p>
+                    <p className="item_info">TEMPLATES</p>
+                </div>
+                <div className="item">
+                    <p className="stats item_info">12M+</p>
+                    <p className="item_info">QUERIES</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+
+export default Home
